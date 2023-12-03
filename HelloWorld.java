929eb7b9ee099f2471783bef5cbe32ef04075037
BekmyrzaTash <152526170+BekmyrzaTash@users.noreply.github.com>
2023-12-03 23:55:40 +0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+    }
+}
+//tried to  upload a code from IntelilJ IDEA, faced some troubles with git pull origin main, so the initial code is in a master branch, working on fixing that)
