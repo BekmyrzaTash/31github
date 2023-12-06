aa6870ae8fae0b44e962b6d85ed4fc90bed8b492
BekmyrzaTash <152526170+BekmyrzaTash@users.noreply.github.com>
2023-12-06 22:22:50 +0600

    Add files via upload

---
+++ Loops1.java
@@ -0,0 +1,13 @@
+public class Loops1 {
+    public static void main(String[]args) {
+        int i =0;
+                while(i<7){
+                    System.out.println("Cycle while"+i);
+                    i++;
+                }
+                System.out.println();
+                for (int j=0; j<7; j++){
+                    System.out.println("Cycle for: " + j);
+                }
+    }
+}
