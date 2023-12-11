42ec364288c022c4ade272706abe49d640a4c9cd
BekmyrzaTash <152526170+BekmyrzaTash@users.noreply.github.com>
2023-12-11 18:30:43 +0100

    Add files via upload

---
+++ DataTypes1.java
@@ -0,0 +1,26 @@
+public class DataTypes1 {
+    public static void main (String[]args){
+
+        byte byteVar=127;
+        short shortVar=32567;
+        int intVar=2135768957;
+        long longVar=2746749947078086L;
+
+        float floatVar=3.14f;
+        double doubleVar=3.141592655359;
+
+        boolean booleanVar= true;
+
+        char charVar='A';
+
+        System.out.println("Byte Variable:"+byteVar);
+        System.out.println("Short Variable:"+shortVar);
+        System.out.println("Int Variable:"+intVar);
+        System.out.println("Long Variable:"+longVar);
+        System.out.println("Float Variable:"+ floatVar);
+        System.out.println("Double Variable:"+ doubleVar);
+        System.out.println("Boolean Variable: "+ booleanVar);
+        System.out.println("Char Variable:"+ charVar);
+
+    }
+}
